c1086d9af65912b3ccf260b9002199348423bc3b
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-05 23:02:05 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,15 +6,15 @@ end
    my_two_array = ["element1", "element2"]
  end
  
- def first_element
+ def first_element(my_new_array)
    my_new_array.first
  end
  
- def third_element
+ def third_element(my_new_array)
    my_new_array[2]
  end
   
-def last_element
+def last_element)(my_new_array)
   my_new_array.last
 end
 
